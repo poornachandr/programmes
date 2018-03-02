cafd7d6d4802c310d5968ea6fc74075307fa9b02
poornachandr <31505561+poornachandr@users.noreply.github.com>
2018-03-02 16:15:29 +0530

    Create p63.c

---
+++ p63.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ char a[50];
+ int i,count=0;
+ printf("\n enter a sentence:");
+scanf("%s",&a[i]);
+for(i=0;a[i]!='\0';i++)
+{
+ count++;
+}
+printf("\n the no of words is %d",count+1);
+return 0;
+}
+
+
