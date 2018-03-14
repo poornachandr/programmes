2bde7a0469d3c0b056447f7f15d1d12dd697771a
poornachandr <31505561+poornachandr@users.noreply.github.com>
2018-03-14 15:58:21 +0530

    Update p76.c

--- p76.c
+++ p76.c
@@ -2,22 +2,23 @@
 int main()
 {
  int num,i,count=0;
- printf("/n enter the number");
- scanrf("%d",&num);
- for(i=1;1<=num;i++)
+ printf("\n enter the number");
+ scanf("%d",&num);
+ for(i=1;i<=num;i++)
  {
   if(num%i==0)
   {
    count++;
   }
+  }
   if(count==2)
   {
-   printf("/n not a composite number");
+   printf("\n not a composite number");
   }
   else
   {
-   printf("/n composite number");
+   printf("\n composite number");
   }
- }
+ 
  return 0;
 } 
