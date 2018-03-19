f98176331e021b48a42e307c3768746b5578055b
poornachandr <31505561+poornachandr@users.noreply.github.com>
2018-03-19 16:49:37 +0530

    Create p110.c

---
+++ p110.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+int main()
+{
+ int a,sum=0,num;
+ printf("\n enter a number");
+ scanf("%d",&num);
+ for(a=1;a<=num;a++)
+ {
+  sum=sum+a;
+ }
+ printf("\n sum is %d",sum);
+ return 0;
+} 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
